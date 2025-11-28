72e8d8d8d4290a33ea132c9da7a210cb69ce8d6f
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-11-28 09:15:32 +0500

    'urls '

--- games/urls.py
+++ games/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Game_view
 
 urlpatterns = [
-    path('games/', Game_view, name='games'),
+    path('games/', Game_view.as_view(), name='games'),
     path('games/<int:id>/', Game_view.as_view(), name='game_detail'),
 ]
